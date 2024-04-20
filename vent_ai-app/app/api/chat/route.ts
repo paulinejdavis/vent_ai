6fc7ff568ca1fbbf5e192383ec441181c3b6f696
pauline <pauljdavi.t21@btinternet.com>
2024-04-20 13:51:19 +0100

    project setup

---
+++ vent_ai-app/app/api/chat/route.ts
@@ -0,0 +1,28 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const openai = new OpenAI({
+  baseURL: "http://127.0.0.1:8000/v1",
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          `You are a professional counsellor who has been hired to respond with care, sympathy and kindness. The responses should be natural, candid, and thought-provoking. They should explore a variety of themes and ideas for mental health challenges, life goals and general chit chatter. Each response should be unique and memorable, with compelling engagement and natural dialogue.`,
+      },
+      ...messages,
+    ],
+  });
+
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
---
+++ vent_ai-app/app/page.tsx
@@ -0,0 +1,121 @@
+//import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, append, isLoading } = useChat();
+  const genres = [
+    { emoji: "🐼", value: "Mavis" },
+    { emoji: "🐻", value: "Candice" },
+    { emoji: "🦅", value: "David" },
+    { emoji: "🦋", value: "Anna" },
+  ];
+  const tones = [
+    { emoji: "😊", value: "Cut to the chase" },
+    { emoji: "😢", value: "Mumsy" },
+    { emoji: "😏", value: "Deep and spiritual" },
+    { emoji: "😂", value: "chat with a friend" },
+  ];
+
+  const [state, setState] = useState({
+    genre: "",
+    tone: "",
+  }); 
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  return (
+    <main className="mx-auto w-full p-24 flex flex-col">
+      <div className="p4 m-4">
+        <div className="flex flex-col items-center justify-center space-y-8 text-white">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold">vent_ai App</h2>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              Choose a buddy that resonates with you
+            </p>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Chat</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {genres.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    value={value}
+                    name="genre"
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Tones</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {tones.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="tone"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            disabled={isLoading || (!state.buddy || !state.tone)}
+            onClick={() =>
+              append({
+                role: "user",
+                content: `Generate a ${state.buddy} chat in a ${state.tone} tone`,
+              })
+            }
+          >
+            Generate Chat
+          </button>
+
+          <div
+            hidden={
+              messages.length === 0 ||
+              messages[messages.length - 1]?.content.startsWith("Generate")
+            }
+            className="bg-opacity-25 bg-gray-700 rounded-lg p-4"
+          >
+            {messages[messages.length - 1]?.content}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
